113208a25752e76bed818dc19af52e8d72a66830
Dante-Cand <cand.dante@gmail.com>
2022-01-12 16:44:41 +0100

    collection klaar

---
+++ collection-project/collection.js
@@ -0,0 +1,106 @@
+const collection = [
+    {
+      name: 'Ratchet & Clank 3',
+      developper: 'Insomniac Games',
+      releaseYear: 2004,
+      picture: '../assets/ratchet & clank 3.jpg',
+      genre: ['platformer', 'third-person shooter'],
+      score: ['4.5/5']
+    },
+    {
+        name: 'Factorio',
+        developper: 'Wube Software',
+        releaseYear: 2016,
+        picture: '../assets/Factorio.png',
+        genre: ['Automation', 'Simulation','Action'],
+        score: ['4.9/5']
+      },
+      {
+        name: 'Hearts of Iron 4',
+        developper: 'Paradox Development Studio',
+        releaseYear: 2016,
+        picture: '../assets/hoi4.jpg',
+        genre: ['Grand strategy wargame', 'Simulation', 'Strategy'],
+        score: ['4.5/5']
+      },
+      {
+        name: 'Company of Heroes 2',
+        developper: 'Relic Entertainment',
+        releaseYear: 2013,
+        picture: '../assets/company of heroes.jpeg',
+        genre: ['Real-time strategy', 'Strategy'],
+        score: ['4.2/5']
+      },
+      {
+        name: 'Europa Universalis 4',
+        developper: 'Paradox Development Studio',
+        releaseYear: 2013,
+        picture: '../assets/eu4.jfif',
+        genre: ['Grand strategy wargame', 'Simulation','Strategy'],
+        score: ['4.5/5']
+      },
+      {
+        name: 'Total war: Rome 2',
+        developper: 'Creative Assembly',
+        releaseYear: 2013,
+        picture: '../assets/rome total war.jpg',
+        genre: ['Real-time strategy', 'Turn-based strategy'],
+        score: ['4.4/5']
+      },
+      {
+        name: 'Napoleon:Total war',
+        developper: 'Creative Assembly',
+        releaseYear: 2010,
+        picture: '../assets/napoleon total war.jpg',
+        genre: ['Real-time strategy', 'Turn-based strategy'],
+        score: ['4.1/5']
+      },
+      {
+        name: 'Modern Warfare 2',
+        developper: 'Infinity Ward',
+        releaseYear: 2009,
+        picture: '../assets/mw2.png',
+        genre: ['First-person shooter', 'Action'],
+        score: ['4.7/5']
+      },
+      {
+        name: 'MAG',
+        developper: 'Zipper Interactive',
+        releaseYear: 2010,
+        picture: '../assets/MAGgame.jpg',
+        genre: ['First-person shooter', 'MMO'],
+        score: ['4.9/5']
+      },
+      {
+        name: 'Doorkickers 2',
+        developper: 'KillHouse Games',
+        releaseYear: 2020,
+        picture: '../assets/doorkickers 2.webp',
+        genre: ['Action', 'Strategy', 'Simulation'],
+        score: ['4.8/5']
+      },
+      {
+        name: 'Enlisted',
+        developper: 'Darkflow Software',
+        releaseYear: 2021,
+        picture: '../assets/enlisted.jpg',
+        genre: ['First-person shooter', 'Simulation'],
+        score: ['4.1/5']
+      },
+      {
+        name: 'Club Penguin',
+        developper: 'RocketSnail Games',
+        releaseYear: 2005,
+        picture: '../assets/club penguin.jpg',
+        genre: ['MMORPG', 'Platformer'],
+        score: ['4.7/5']
+      },
+      {
+        name: 'Enemy Territory: Quake Wars',
+        developper: 'Splash Damage',
+        releaseYear: 2007,
+        picture: '../assets/enemy-territory-quake-wars.jpg',
+        genre: ['First-person shooter', 'Real-time strategy'],
+        score: ['4.4/5']
+      },
+  ];
