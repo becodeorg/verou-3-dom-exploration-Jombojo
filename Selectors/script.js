f0fa6688c7d7eab07ec2ab79635a9b09f41e448a
Dante-Cand <cand.dante@gmail.com>
2022-01-12 10:14:16 +0100

    begin oef 3 2de poging

--- Selectors/script.js
+++ Selectors/script.js
@@ -1,5 +1,4 @@
-function generateRandomColor()
-{
+function generateRandomColor() {
     var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
     return randomColor;}
 //Add a title attribute to every element that has the important class, stating This is an important item
